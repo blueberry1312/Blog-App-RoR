54629330d740dd4229c1defafc514c88eedd7146
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-06-16 00:58:22 -0400

    Add testfile for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,22 +3,38 @@ require 'rails_helper'
 RSpec.describe 'UsersController', type: :request do
   describe 'GET index' do
     it 'renders the index template' do
-        get '/users'
-        expect(response).to render_template(:index)
-      end
+      get '/users'
+      expect(response).to render_template(:index)
+    end
 
-      it 'returns a successful response' do
-        get '/users'
-        expect(response).to have_http_status(:success)
-      end
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
 
-      it 'includes the correct placeholder text in the response body' do
-        get '/users'
-        expect(response.body).to include('Here is a list of users')
-      end
+    it 'includes the correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('Here is a list of users')
+    end
   end
 
   describe 'GET show' do
-    # Add your examples for the show action in UsersController
+    it 'renders the show template' do
+      user = User.create(name: 'Test', photo: 'https://test.jpg', bio: 'test.')
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns a successful response' do
+      user = User.create(name: 'Test', photo: 'https://test.jpg', bio: 'test.')
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      user = User.create(name: 'Test', photo: 'https://test.jpg', bio: 'test.')
+      get "/users/#{user.id}"
+      expect(response.body).to include('Showing a specific User')
+    end
   end
 end
