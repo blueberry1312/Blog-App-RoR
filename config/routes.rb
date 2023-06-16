4ea67950a923a6eed41ef0c7fd30d708b28a2e83
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-06-16 01:08:47 -0400

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
 
 end
