f7607b25e62c91a22a201d34290981e1566e9f22
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-06-16 04:22:23 -0400

    Add users_show view

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,11 +25,5 @@ RSpec.describe 'UsersController', type: :request do
       get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
     end
-
-    it 'includes the correct placeholder text in the response body' do
-      user = User.create(name: 'Test', photo: 'https://test.jpg', bio: 'test.')
-      get "/users/#{user.id}"
-      expect(response.body).to include('Showing a specific User')
-    end
   end
 end
